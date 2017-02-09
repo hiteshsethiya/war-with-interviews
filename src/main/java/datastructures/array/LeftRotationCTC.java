41c55976fb0152eeef37eee06609d2e9f636001e
hiteshsethiya <hiteshsethiya@gmail.com>
2017-02-10 00:37:19 +0530

    array rotation, 2nd method

---
+++ src/main/java/datastructures/array/LeftRotationCTC.java
@@ -0,0 +1,39 @@
+package datastructures.array;
+
+import java.util.Scanner;
+
+/**
+ * Created by racit-2105 on 10/02/17.
+ * 2nd method using a new array and index shifts
+ */
+public class LeftRotationCTC {
+    public static int[] arrayLeftRotation(int[] a, int n, int k) {
+        int[] out = new int[n];
+        for(int i = 0; i < n; ++i) {
+            if(i - k < 0) {
+                out[n + i - k] = a[i];
+            } else {
+                out[i - k] = a[i];
+            }
+        }
+        return out;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+
+        int[] output = new int[n];
+        output = arrayLeftRotation(a, n, k);
+        for(int i = 0; i < n; i++)
+            System.out.print(output[i] + " ");
+
+        System.out.println();
+
+    }
+}
